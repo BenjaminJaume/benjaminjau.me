8ae8c4579f85c98331ee39b99f6f66c6603a1046
BenjaminJaume <benjamin.jaume@gmail.com>
2020-01-10 07:47:38 -0600

    test connection db api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const express = require("express");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const pg = require("pg");
+const cors = require("cors");
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 5000;
 
 // Multi-process to utilize all CPU cores.
@@ -15,28 +17,65 @@ if (!isDev && cluster.isMaster) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
   });
-
 } else {
   const app = express();
+  app.use(cors());
+
+  //Database Config .env
+  const config = {
+    user: process.env.PG_USER,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASS,
+    port: process.env.PG_PORT,
+    host: process.env.PG_HOST,
+    ssl: true,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: { require: false }
+    }
+  };
+
+  //Documentation for node-postgres: https://node-postgres.com/
+  const pool = new pg.Pool(config);
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
   // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+  app.get("/api.json", function(req, res) {
+    res.set("Content-Type", "application/json");
+    pool.connect(function(err, client, done) {
+      if (err) {
+        console.log("Can not connect to the DB because of " + err);
+      }
+      client.query("SELECT * FROM projects", function(err, result) {
+        done();
+        if (err) {
+          console.log(err);
+          res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
+      });
+    });
   });
 
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  app.get("*", function(request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../react-ui/build", "index.html")
+    );
   });
 
-  app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  app.listen(PORT, function() {
+    console.error(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
   });
 }
